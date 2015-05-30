68d1720d22fc60b46decd2dbbc5a966dfca46eaf
Mohammad Anwari <mdamt@mdamt.net>
2015-05-31 00:00:11 +0700

    Checks err

--- test/index.js
+++ test/index.js
@@ -7,6 +7,7 @@ describe("DscGrabber", function() {
   it("parses a plain dsc", function(done) {
     dsc = new DscGrabber(data + "dsc-without-pgp.dsc");
     dsc.parse(function(err, parsed) {
+      assert.strictEqual(err, null);
       assert.strictEqual(parsed.format, "3.0 (native)");
       done();
     });
@@ -15,6 +16,7 @@ describe("DscGrabber", function() {
   it("parses a dsc enclosed in PGP body", function(done) {
     dsc = new DscGrabber(data + "dsc-with-pgp.dsc");
     dsc.parse(function(err, parsed) {
+      assert.strictEqual(err, null);
       assert.strictEqual(parsed.format, "1.0");
       done();
     });
